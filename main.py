1135454d142d9d53277d5be67f1057deec695ab3
RCoff <enfaduraa@gmail.com>
2021-05-12 22:20:55 -0400

    Add retrying, update logging

--- main.py
+++ main.py
@@ -1,14 +1,121 @@
 import pandas as pd
+import tweepy  # Twitter API
+from pythonjsonlogger import jsonlogger
 
 import logging
-import _sqlite3
+import sqlite3
+from typing import Any
+import time
 
 import config
 
 
 def main():
-    pass
+    logging.info("Starting")
+
+    auth = tweepy.AppAuthHandler(config.twitter_api_key, config.twitter_api_secret_key)
+    api = tweepy.API(auth)
+
+    query = """yellowstone -from:YellowstoneNPS -from:WYellowstoneMT 
+               -from:DestYellowstone -is:retweet -is:reply -is:nullcast
+            """
+
+    conn = sqlite3.connect("Yellowstone.db")
+    df = pd.read_sql("SELECT * FROM Tweets", con=conn)
+
+    results_list = []
+    # Use recursion to retrieve tweets until no more are returned
+    results_list = get_search_results(query, api, geocode="44.5071129,-111.1972906,150mi", result_type="recent",
+                                      count=100, tweet_mode="extended", max_id=None)
+
+    # results = api.search(query, geocode="44.5071129,-111.1972906,150mi", result_type="recent", count=100,
+    #                      tweet_mode="extended", max_id=None)
+    # for result in results:
+    #     results_list.append(result)
+    #
+    # while results.since_id:
+    #     results = api.search(query, geocode="44.5071129,-111.1972906,100mi", result_type="recent", count=100,
+    #                          tweet_mode="extended", max_id=results.max_id)
+    #     if results:
+    #         for result in results:
+    #             results_list.append(result)
+
+    rows_added_counter: int = 0
+    for tweet in results_list:
+        if tweet.id not in df['tweet_id'].tolist():
+            query = """
+                INSERT INTO Tweets (tweet_id, text, user_id, user_handle, source, source_url, created_datetime)
+                    VALUES(?, ?, ?, ?, ?, ?, ?)
+            """
+
+            values = (
+                tweet.id,
+                tweet.full_text,
+                tweet.user.id,
+                tweet.user.screen_name,
+                tweet.source,
+                tweet.source_url,
+                tweet.created_at
+            )
+
+            conn.execute(query, values)
+            conn.commit()
+            rows_added_counter += 1
+
+    logging.info("Operation Completed", extra={'Rows_Added': rows_added_counter})
+
+
+def get_search_results(query: str, api: tweepy.api, **kwargs: Any) -> list:
+
+    # results: Any = None
+    results_list: list = []
+
+    for i in range(3):
+        try:
+            results = api.search(query, **kwargs)
+
+            # Need to handle rate limit here
+        except:
+            logging.exception(f"An exception has occurred. Retrying... {i}", extra={'Retry': i})
+            time.sleep(5 + (i * 5))
+            continue
+        break
+    else:
+        logging.critical("Not able to get results", extra={'Retries': i})
+        return results_list
+
+    results_list = [result for result in results]
+
+    if results.max_id:
+        kwargs['max_id'] = results.max_id
+        results_list.extend(get_search_results(query, api, **kwargs))
+
+    return results_list
+
+
+def setup_logging():
+    logging.basicConfig(filename='log.log', level=logging.DEBUG)
+
+
+def setup_database():
+    conn = sqlite3.connect("Yellowstone.db")
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS Tweets (
+            tweet_id INTEGER PRIMARY KEY,
+            text TEXT DEFAULT NULL,
+            user_id INTEGER DEFAULT NULL,
+            user_handle TEXT DEFAULT NULL,
+            source TEXT DEFAULT NULL,
+            source_url TEXT DEFAULT NULL,
+            created_datetime TEXT DEFAULT NULL
+        )
+    """)
+
+    conn.commit()
 
 
 if __name__ == "__main__":
+    setup_logging()
+    setup_database()
     main()
